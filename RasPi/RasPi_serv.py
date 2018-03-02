a57cb5be989713cfdd0ce89b503ee883dff37b68
Flotchie <bertrand.latombe@free.fr>
2018-03-02 19:40:28 +0100

    bascule en serveur http

--- RasPi/RasPi_serv.py
+++ RasPi/RasPi_serv.py
@@ -5,12 +5,13 @@ import logging
 import logging.handlers
 import argparse
 import sys
-import zmq
 import os
 from os.path import isfile, join
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
 
 # Defaults
-zmqPort = 5556
+httpPort = 4567
 relayDir='/home/pi/data/relay/'
 tProbeDir='/home/pi/data/tprobe/'
 LOG_FILENAME = "/home/pi/log/RasPi_serv.log"
@@ -33,6 +34,7 @@ formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 # Attach the handler to the logger
 logger.addHandler(handler)
+
 
 # Make a class we can use to capture stdout and sterr in the log
 class MyLogger(object):
@@ -125,76 +127,94 @@ def listTProbe():
       if isfile(join(tProbeDir, f)):
          l = l + " " + f
    return l
-   
+
+class S(BaseHTTPRequestHandler):
+   def _set_headers(self): 
+      self.send_response(200)
+      self.send_header('Content-type', 'text/json')
+      self.end_headers()
+
+   def do_GET(self):
+      print "GET"
+      self._set_headers()
+      message = self.path[1:]
+      logger.info("Received request: "+ message)
+      args = message.split("/")
+      if args[0] == "READ":
+         value=""
+         if len(args)==2:
+            try:
+               relayId = int(args[1])
+               value=readRelay(relayId)
+            except:
+               pass
+         logger.info( " -> %s" % value)
+         self.wfile.write("{\"data\":%s}"%(value))     
+      elif args[0] == "WRITE":
+         if len(args)==3:
+            try:      
+               relayId = int(args[1])
+               value = int(args[2])
+               writeRelay(relayId, value)
+            except:
+               pass
+         self.wfile.write("{\"data\":%d}"%(value))   
+      elif args[0] == "DS18B20" or args[0] == "TEMP":
+         temp=""
+         if len(args)==2:
+            temp=readTemp(args[1])
+         logger.info( " -> %s" % temp)
+         self.wfile.write("{\"data\":%s}"%(temp))
+      elif args[0] == "TEMP_QUALITY":
+         temp=""
+         if len(args)==2:
+            temp=readTempQuality(args[1])
+         logger.info( " -> %s" % temp)
+         self.wfile.write("{\"data\":%s}"%(temp))
+      elif args[0] == "TEMP_DATE":
+         temp=""
+         if len(args)==2:
+            temp=readTempDate(args[1])
+         logger.info( " -> %s" % temp)
+         self.wfile.write("{\"data\":%s}"%(temp))
+      elif args[0] == "TPROBE":
+         datas = listTProbe()
+         self.wfile.write("{\"data\":%s}"%(datas))
+      elif args[0] == "STOP":
+         logger.info("Stopping...")
+         StopServer=True
+      else:
+         logger.error("unknown command ["+args[0]+"]")
+         self.wfile.write("{\"data\":""unknown command [%s]""}"%(args[0]))
+ 
+   def do_HEAD(self):
+      print "HEAD"
+      self._set_headers()
+
+   def do_POST(self):
+      print "POST"   
+      # Doesn't do anything with posted data
+      self._set_headers()
+      self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+
 # Replace stdout with logging to file at INFO level
-sys.stdout = MyLogger(logger, logging.INFO)
+#sys.stdout = MyLogger(logger, logging.INFO)
 # Replace stderr with logging to file at ERROR level
-sys.stderr = MyLogger(logger, logging.ERROR)
+#sys.stderr = MyLogger(logger, logging.ERROR)
 
 # remise à 0 des Relais
 for relayId in range(1,9):
    writeRelay(relayId, 0)
    
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:%d" % zmqPort)
+server_address = ('', httpPort)
+httpd = HTTPServer(server_address, S)
+print 'Starting httpd...'
 
 StopServer=False
 while not StopServer:
    #  Wait for next request from client
-   message = socket.recv()
-   logger.info("Received request: "+ message)
-   args = message.split()
-   if args[0] == "READ":
-      value=""
-      if len(args)==2:
-         try:
-            relayId = int(args[1])
-            value=readRelay(relayId)
-         except:
-            pass
-      logger.info( " -> %s" % value)
-      socket.send(value)     
-   elif args[0] == "WRITE":
-      if len(args)==3:
-         try:      
-            relayId = int(args[1])
-            value = int(args[2])
-            writeRelay(relayId, value)
-         except:
-            pass            
-      socket.send("")
-   elif args[0] == "DS18B20" or args[0] == "TEMP":
-      temp=""
-      if len(args)==2:
-         temp=readTemp(args[1])
-      logger.info( " -> %s" % temp)
-      socket.send(temp)
-   elif args[0] == "TEMP_QUALITY":
-      temp=""
-      if len(args)==2:
-         temp=readTempQuality(args[1])
-      logger.info( " -> %s" % temp)
-      socket.send(temp)   
-   elif args[0] == "TEMP_DATE":
-      temp=""
-      if len(args)==2:
-         temp=readTempDate(args[1])
-      logger.info( " -> %s" % temp)
-      socket.send(temp)       
-   elif args[0] == "TPROBE":
-      datas = listTProbe()
-      socket.send(datas)     
-   elif args[0] == "STOP":
-      logger.info("Stopping...")
-      socket.send("")
-      StopServer=True
-   else:
-      logger.error("unknown command ["+args[0]+"]")
-      socket.send("unknown command ["+args[0]+"]" )
+   httpd.handle_request()
 
 # remise à 0 des Relais
 for relayId in range(1,9):
    writeRelay(relayId, 0)
-
-	
